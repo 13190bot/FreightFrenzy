4e7ba0cfb6fdb62f4e252068328007e6d03a5e05
mhuynh5 <melvinhuynh5@gmail,com>
2021-10-27 14:44:20 -0700

    Created TankDrive and finished it

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class TankDrive extends LinearOpMode {
+
+    private DcMotor leftMotor, rightMotor;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        leftMotor = hardwareMap.get(DcMotor.class, "left_motor");
+        rightMotor = hardwareMap.get(DcMotor.class, "right_motor");
+
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double lefty = this.gamepad1.left_stick_y;
+            double rightx = this.gamepad1.right_stick_x/2;
+
+            double denominator = Math.max(Math.abs(lefty) + Math.abs(rightx), 1);
+            double leftPower = (-lefty + rightx) / denominator;
+            double rightPower = (lefty + rightx) / denominator;
+
+            leftMotor.setPower(leftPower);
+            rightMotor.setPower(rightPower);
+
+            telemetry.addData("LeftMotor", leftPower);
+            telemetry.addData("RightMotor", rightPower);
+            telemetry.update();
+        }
+    }
+}
