c347c5617cb6620a8c6ae022a312e803719636d4
Team 8872 <teamrobotics8872@gmail.com>
2021-11-02 17:39:07 -0700

    Created outlines for freightfrenzy(autonomous13190, DuckAutonomous13190) started ClawTeleOp added ClawAutonomous class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightfrenzy/DuckAutonomous13190.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.freightfrenzy;
+
+public class DuckAutonomous13190 {
+    /*
+    This program will operate the robot to just score the ducks and park in case the alliance robot has better autonomous
+     */
+
+    //initialize encoders and motors for Autonomous
+
+    //TODO follow the wall to the duck station so the robot doesn't collide with the alliance robot
+
+    //TODO adjust the robot's position so it can rotate the plate
+
+    //TODO do the duck mission before it reaches time
+
+    //TODO move toward the parking space (If we have deadWheals, we have to make sure it doesn't break when the robot goes over the ramp)
+    //moving over the ramps and go further
+}
