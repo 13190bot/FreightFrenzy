11cfe17bf8782defb9470908bd413778572f70be
mhuynh5 <melvinhuynh5@gmail,com>
2021-11-08 17:31:08 -0800

    created AdvancedAutonomous13190 class and outline

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightfrenzy/AdvancedAutonomous13190.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.freightfrenzy;
+
+public class AdvancedAutonomous13190 {
+    /*
+    This program will be containing the advanced plan we have for autonomous
+    Do the original plan first and then work on this
+     */
+
+    //TODO Do the preloaded process
+
+    //cap the object(if we are going to use it)
+
+    //TODO Do the object transportation
+
+    //TODO Park
+}
