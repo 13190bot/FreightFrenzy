a87aa0ffc46b55717863cc31a9d12d1a821cb98a
mhuynh5 <melvinhuynh5@gmail,com>
2021-11-16 17:43:47 -0800

    started to write ClawAutonomous class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw/ClawTeleOpMini.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw/ClawTeleOpMini.java
@@ -3,16 +3,19 @@ package org.firstinspires.ftc.teamcode.Claw;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
 public class ClawTeleOpMini extends LinearOpMode {
 
     private Servo servo;
+    private DcMotor dc;
 
     @Override
     public void runOpMode() {
 
         servo = hardwareMap.servo.get("servo");
+        dc = hardwareMap.dcMotor.get("DcMotor");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -22,11 +25,8 @@ public class ClawTeleOpMini extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            if(gamepad1.a) {
-                servo.setPosition(0.5);
-            } else if (gamepad1.b) {
-                servo.setPosition(1);
-            }
+
+
 
             telemetry.addData("Servo Position", servo.getPosition());
             telemetry.addData("Status", "Running");
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawAutonomous.java
@@ -1,4 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
-public class ClawAutonomous {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name="ClawAutonomous", group="LinearOpMode")
+//@Disabled
+public class ClawAutonomous extends LinearOpMode {
+
+    private Servo servo1, servo2, servo3;
+    private DcMotor dc;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        servo1 = hardwareMap.servo.get("servoBottom");
+        servo2 = hardwareMap.servo.get("servoMiddle");
+        servo3 = hardwareMap.servo.get("servoTop");
+
+        dc = hardwareMap.dcMotor.get("dcMotor");
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        if (opModeIsActive()) {
+
+        }
+    }
 }
