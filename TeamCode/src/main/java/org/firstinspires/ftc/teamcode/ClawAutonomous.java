a3c8c56144ab3ebad7abb6f0e641029a23ddeac6
mhuynh5 <melvinhuynh5@gmail,com>
2021-11-18 17:28:54 -0800

    wrote pickUpObject and started dropAtLevel method of ClawAutonomous class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawAutonomous.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
@@ -20,14 +21,44 @@ public class ClawAutonomous extends LinearOpMode {
         servo3 = hardwareMap.servo.get("servoTop");
 
         dc = hardwareMap.dcMotor.get("dcMotor");
+        dc.setDirection(DcMotor.Direction.REVERSE);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         waitForStart();
 
-        if (opModeIsActive()) {
+        pickUpObject();
+        dropAtLevel(1);
+    }
 
+    public void pickUpObject() throws InterruptedException {
+        if (opModeIsActive()) {
+            //TODO have to modify the power and time in order to move claw properly
+            //go down
+            servo1.setPosition(0.083);
+            servo2.setPosition(0);
+            servo3.setPosition(1);
+            sleep(500);
+            dc.setPower(0.2);
+            sleep(500);
+            dc.setPower(0);
+
+            //pick up
+            servo3.setPosition(0);
+            sleep(1000);
+            dc.setPower(-0.2);
+            sleep(500);
+            dc.setPower(0);
+
+            telemetry.addData("Servo1 Position", servo1.getPosition());
+            telemetry.addData("Servo2 Position", servo2.getPosition());
+            telemetry.addData("Claw status", "Opening");
+            telemetry.update();
         }
+    }
+
+    public void dropAtLevel(int level) throws InterruptedException {
+
     }
 }
