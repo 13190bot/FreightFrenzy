2f95c4f6f3e59170b4899bb2d3ce6b8c86320b55
wilcoxrobotics <teamrobotics8872@gmail.com>
2021-12-08 17:13:29 -0800

    slightly working code!! the arm moved

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawTestThing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawTestThing.java
@@ -10,46 +10,60 @@ import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_TO_POSITION;
 //and the servo can rotate 180 degrees with the maximum/minimum pointing straight down
 @TeleOp
 public class ClawTestThing extends LinearOpMode {
-    static final double COUNTS_PER_MOTOR_REV = 384.5;
+    static final double COUNTS_PER_MOTOR_REV = 1992.6;
     static final double MAX_POSITION = 1.0;
     static final double MIN_POSITION = 0.0;
     static double motorPosition = 180;
 
     private DcMotor motor;
-    private Servo servo;
+    double encoderOutput = 0;
+    //private Servo servo;
 
     @Override
     public void runOpMode(){
         motor = hardwareMap.get(DcMotor.class, "claw_motor");
-        servo = hardwareMap.get(Servo.class, "claw_servo_bottom");
-        motor.setMode(RUN_TO_POSITION);
+        //servo = hardwareMap.get(Servo.class, "claw_servo_bottom");
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
         while(opModeIsActive()) {
-            if (gamepad1.b) {
-                if (motorPosition < 100) {
-                    continue;
-                }
+            encoderOutput = motor.getCurrentPosition();
+            telemetry.addData("Encoder Position", encoderOutput);
+            telemetry.addData("Motor Power", 0);
+            telemetry.update();
+            if(gamepad1.b){
+                telemetry.addData("Running",true);
+                telemetry.update();
+            }
+            if (gamepad1.left_stick_x<-0.5) {
                 moveDcMotor(motorPosition - 45);
             }
-            if (gamepad1.x) {
-                if (motorPosition > 215) {
-                    continue;
-                }
-                moveDcMotor(motorPosition +45);
+            if (gamepad1.left_stick_x > 0.5) {
+                moveDcMotor(motorPosition + 45);
             }
         }
     }
 
     public void moveDcMotor(double angle){
+        telemetry.addData("Moving", true);
+        telemetry.update();
         double angleChange = angle-motorPosition;
         motorPosition = angle;
         int movementAmount = (int) (angleChange*COUNTS_PER_MOTOR_REV)/360;
         motor.setTargetPosition(movementAmount);
-        while(motor.isBusy()){
-            sleep(50);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor.setPower(1);
+        while(opModeIsActive() && motor.isBusy()){
+            telemetry.addData("Motor Power", motor.getPower());
+            telemetry.update();
         }
+        motor.setPower(0);
+        telemetry.addData("Motor Power", motor.getPower());
+        telemetry.update();
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
+    /*
     public void setAngle(double angle){
         double angleChange = angle-motorPosition;
         motorPosition = angle;
@@ -61,7 +75,7 @@ public class ClawTestThing extends LinearOpMode {
             sleep(50);
         }
     }
-
+*/
     /* currently useless
     public void resetPosition(){
         servo.setPosition(0);
