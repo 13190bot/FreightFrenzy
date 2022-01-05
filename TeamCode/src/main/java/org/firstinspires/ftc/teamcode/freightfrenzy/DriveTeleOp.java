860f17cf2be819327a023e10048c82990c1a8c1e
mhuynh5 <melvinhuynh5@gmail,com>
2022-01-04 17:39:09 -0800

    changed DriveTeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightfrenzy/DriveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightfrenzy/DriveTeleOp.java
@@ -1,17 +1,19 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.freightfrenzy;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp
+@TeleOp(name="DriveTeleOp", group = "TeleOpCode")
 public class DriveTeleOp extends LinearOpMode {
-    private void runOpMode() throws InterruptedException{
-        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("front_left_motor"));
-        DcMotor backLeftMotor = hardwareMap.dcMotor.get("front_left_motor"));
-        DcMotor frontRightMotor = hardwareMap.dcMotor.get("back_right_motor"));
-        DcMotor backRightMotor = hardwareMap.dcMotor.get("back_left_motor");
+
+    private DcMotor frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor;
+
+    public void runOpMode() throws InterruptedException{
+        frontLeftMotor = hardwareMap.dcMotor.get("front_left_motor");
+        backLeftMotor = hardwareMap.dcMotor.get("front_left_motor");
+        frontRightMotor = hardwareMap.dcMotor.get("back_right_motor");
+        backRightMotor = hardwareMap.dcMotor.get("back_left_motor");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -19,9 +21,9 @@ public class DriveTeleOp extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            double y = -this.gamepad.left_stick_y;
-            double x = this.gamepad.left_stick_x;
-            double rx = this.gamepad.right_stick_x;
+            double y = -this.gamepad1.left_stick_y;
+            double x = this.gamepad1.left_stick_x;
+            double rx = this.gamepad1.right_stick_x;
 
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double frontLeftPower = (y + x + rx) / denominator;
@@ -29,16 +31,16 @@ public class DriveTeleOp extends LinearOpMode {
             double frontRightPower = (y - x - rx) / denominator;
             double backRightPower = (y + x - rx) / denominator;
 
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+
             telemetry.addData("FrontLeftPower", frontLeftPower);
             telemetry.addData("BackLeftPower", backLeftPower);
             telemetry.addData("FrontRightPower", frontRightPower);
             telemetry.addData("BackRightPower", backRightPower);
             telemetry.update();
-
-            frontLeftMotor.setPower(frontLeftPower);
-            backLeftMotor.setPower(backLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            backRightPower.setPower(backRightPower);
         }
     }
 }
