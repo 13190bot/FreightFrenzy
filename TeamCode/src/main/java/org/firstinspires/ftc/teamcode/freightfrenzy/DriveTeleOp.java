4fdee7ce0a36191768fca5cc956b036aa57d1b46
Team 8872 <teamrobotics8872@gmail.com>
2022-01-21 17:02:45 -0800

    DriveTeleOp updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightfrenzy/DriveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightfrenzy/DriveTeleOp.java
@@ -1,13 +1,16 @@
 package org.firstinspires.ftc.teamcode.freightfrenzy;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 @TeleOp(name="DriveTeleOp", group = "TeleOpCode")
 public class DriveTeleOp extends LinearOpMode {
 
     private DcMotor frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor, duckMotor;
+    private SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
     public void runOpMode() throws InterruptedException{
         frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
@@ -22,30 +25,38 @@ public class DriveTeleOp extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            double y = -this.gamepad1.left_stick_y;
-            double x = this.gamepad1.left_stick_x;
-            double rx = this.gamepad1.right_stick_x;
+            double horizontal = gamepad1.left_stick_x;
+            double vertical = gamepad1.left_stick_y;
+            double angle = gamepad1.right_stick_x;
 
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (-y - rx - x) / denominator;
-            double rearLeftPower = (-y - rx + x) / denominator;
-            double frontRightPower = (y - rx - x) / denominator;
-            double rearRightPower = (y - rx + x) / denominator;
-
-            frontLeftMotor.setPower(frontLeftPower);
-            rearLeftMotor.setPower(rearLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            rearRightMotor.setPower(rearRightPower);
+            drive.setWeightedDrivePower(new Pose2d(vertical, horizontal, angle)); // in roadrunner x is vertical and y is horizontal
+            drive.update();
 
             if (this.gamepad1.left_bumper) {
                 duckMotor.setPower(0.5);
             }
 
-            telemetry.addData("FrontLeftPower", frontLeftPower);
-            telemetry.addData("BackLeftPower", rearLeftPower);
-            telemetry.addData("FrontRightPower", frontRightPower);
-            telemetry.addData("BackRightPower", rearRightPower);
+            telemetry.addData("FrontLeftPower", frontLeftMotor.getPower());
+            telemetry.addData("BackLeftPower", rearLeftMotor.getPower());
+            telemetry.addData("FrontRightPower", frontRightMotor.getPower());
+            telemetry.addData("BackRightPower", rearRightMotor.getPower());
             telemetry.update();
         }
+    }
+    @SuppressWarnings("SameParameterValue")
+    protected void mechanumDrive(boolean xDisable, boolean yDisable) {
+        double horizontal = -gamepad1.left_stick_x;
+        double vertical = -gamepad1.left_stick_y;
+        double angle = -gamepad1.right_stick_x;
+
+        if (xDisable) {
+            horizontal = 0;
+        }
+        if (yDisable) {
+            vertical = 0;
+        }
+
+        drive.setWeightedDrivePower(new Pose2d(vertical, horizontal, angle)); // in roadrunner x is vertical and y is horizontal
+        drive.update();
     }
 }
