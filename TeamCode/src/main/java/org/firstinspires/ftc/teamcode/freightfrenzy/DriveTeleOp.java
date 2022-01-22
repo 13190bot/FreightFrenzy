577e3097db7160be8afd928e66d0236715127456
Team 8872 <teamrobotics8872@gmail.com>
2022-01-21 17:08:49 -0800

    DriveTeleOp updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightfrenzy/DriveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightfrenzy/DriveTeleOp.java
@@ -43,20 +43,4 @@ public class DriveTeleOp extends LinearOpMode {
             telemetry.update();
         }
     }
-    @SuppressWarnings("SameParameterValue")
-    protected void mechanumDrive(boolean xDisable, boolean yDisable) {
-        double horizontal = -gamepad1.left_stick_x;
-        double vertical = -gamepad1.left_stick_y;
-        double angle = -gamepad1.right_stick_x;
-
-        if (xDisable) {
-            horizontal = 0;
-        }
-        if (yDisable) {
-            vertical = 0;
-        }
-
-        drive.setWeightedDrivePower(new Pose2d(vertical, horizontal, angle)); // in roadrunner x is vertical and y is horizontal
-        drive.update();
-    }
 }
