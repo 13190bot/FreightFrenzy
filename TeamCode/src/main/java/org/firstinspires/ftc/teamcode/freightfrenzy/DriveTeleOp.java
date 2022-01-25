38386f9a53aeba4e350dc9ef400fc26e15fcb1b1
paradebot <itsbrian1113@gmail.com>
2022-01-24 18:52:10 -0800

    error fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightfrenzy/DriveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freightfrenzy/DriveTeleOp.java
@@ -7,13 +7,18 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 @TeleOp(name="DriveTeleOp", group = "TeleOpCode")
-public class DriveTeleOp extends LinearOpMode{
+public class DriveTeleOp extends LinearOpMode {
 
     private DcMotor frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor, duckMotor;
-    private SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+    private SampleMecanumDrive drive;
 
     public void runOpMode() throws InterruptedException{
-
+        frontLeftMotor = hardwareMap.dcMotor.get("leftFront");
+        rearLeftMotor = hardwareMap.dcMotor.get("leftRear");
+        frontRightMotor = hardwareMap.dcMotor.get("rightFront");
+        rearRightMotor = hardwareMap.dcMotor.get("rightRear");
+        duckMotor = hardwareMap.dcMotor.get("duckMotor");
+        drive = new SampleMecanumDrive(hardwareMap);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -21,18 +26,15 @@ public class DriveTeleOp extends LinearOpMode{
         waitForStart();
 
         while (opModeIsActive()) {
-            double horizontal = gamepad1.left_stick_x;
-            double vertical = gamepad1.left_stick_y;
-            double angle = gamepad1.right_stick_x;
+            double horizontal = -gamepad1.left_stick_x;
+            double vertical = -gamepad1.left_stick_y;
+            double angle = -gamepad1.right_stick_x;
 
             drive.setWeightedDrivePower(new Pose2d(vertical, horizontal, angle)); // in roadrunner x is vertical and y is horizontal
             drive.update();
 
             if (this.gamepad1.left_bumper) {
-                duckMotor.setPower(0.25);
-            }
-            if(this.gamepad1.right_bumper){
-                duckMotor.setPower(1);
+                duckMotor.setPower(0.5);
             }
 
             telemetry.addData("FrontLeftPower", frontLeftMotor.getPower());
