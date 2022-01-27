07fdaafd2f69b58ce906133787571de248480bd6
Team 8872 <teamrobotics8872@gmail.com>
2022-01-26 16:28:27 -0800

    duck tleeOP

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/objectDetection/DuckLeftMiddle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/objectDetection/DuckLeftMiddle.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 
 @TeleOp
 public class DuckLeftMiddle extends LinearOpMode {
-    private static final String TFOD_MODEL_ASSET = "/sdcard/FIRST/vision/FreightFrenzy_BCDM.tflite";
+    private static final String TFOD_MODEL_ASSET = "/sdcard/FIRST/vision/FreightFrenzy_DM.tflite";
     private static final String[] LABELS = {
             "Duck",
             "Marker"
